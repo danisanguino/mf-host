c6e08d8f8a6317bf989bcedeee044d42dda1e535
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2024-12-05 20:54:24 +0100

    fixed URL real

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const printCompilationMessage = require('./compilation.config.js');
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath: "https://app-colorpicker-sang.netlify.app/",
   },
 
   resolve: {
